1e85345dc11b8e7f1b5dc594449621f3627318aa
SurajMesta <64083712+SurajMesta@users.noreply.github.com>
2020-06-21 17:40:19 +0530

    Update router.js

--- router/router.js
+++ router/router.js
@@ -1,12 +1,13 @@
 const express= require('express')
 const appRouter= express.Router()
 const controller= require('../controller/controller')
+const verify= require('./verifyToken')
 
 
 appRouter.route('/').get(controller.home)
 appRouter.route('/signup').post(controller.newPost)
 appRouter.route('/login').post(controller.login)
-
+appRouter.route('/posts').get(verify,controller.posts)
 
 
 module.exports=appRouter
